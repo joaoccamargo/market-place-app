4f676655c11acb81b7c08a170a9d601e9aaa9c7e
Joao Camargo <jclaudio00_12@hotmail.com>
2025-12-16 19:43:45 -0300

    fix

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Login() {
     return(
         <View>
-            <Text className="text-lg">Login Page</Text>
+            <Text className="text-green-500">Login Page</Text>
         </View>
     )
 }
